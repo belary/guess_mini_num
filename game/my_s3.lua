3967fe11824ce0de2cd98c43ae1dfef7b369048a
belary <16096880@qq.com>
2024-10-31 19:00:28 +0800

    d2

---
+++ game/my_s3.lua
@@ -0,0 +1,18 @@
+local skynet = require("skynet")
+local args = {...}
+if (#args == 0) then
+    table.insert(args, "onlyservice")
+end
+
+skynet.start(function()
+    local us
+    skynet.error("test unique service")
+
+    if (#args == 2 and args[1] == "true") then 
+        us = skynet.uniqueservice(true, args[2])
+    else
+        us = skynet.uniqueservice(args[1])
+    end
+
+    skynet.error("uniqueservice handler:", skynet.address(us))
+end)
---
+++ game/onlyservice.lua
@@ -0,0 +1,4 @@
+local skynet = require("skynet")
+skynet.start(function()
+    skynet.error("this is default service started!!!")
+end)
